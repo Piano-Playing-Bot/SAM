41f96a29a9fe965edaa52e0a57787ecc7c435370
ArtInLines <linesinart@gmail.com>
2023-10-09 21:32:42 +0200

    feat: test drag-n-drop

--- src/main.c
+++ src/main.c
@@ -19,18 +19,27 @@
 #include "stb_ds.h"  // For dynamic arrays
 
 
+typedef enum {
+    UI_VIEW_START,
+    UI_VIEW_FILE,
+} UI_View;
+
 int main(void)
 {
-    i32 win_width  = 1200;
-    i32 win_height = 600;
+    i32 winWidth  = 1200;
+    i32 winHeight = 600;
+
+    UI_View view = UI_VIEW_START;
+    char *startViewMsg = "Drag-and-Drop a MIDI-File to play it on the Piano";
+    char *filePath = NULL;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(win_width, win_height, "RL");
+    InitWindow(winWidth, winHeight, "RL");
     SetTargetFPS(60);
 
-    // float spacing      = 2;
-    // float padding      = 5;
-    // float margin       = 8;
+    float spacing      = 2;
+    float padding      = 5;
+    float margin       = 8;
     float size_default = 50;
     float size_max     = size_default;
     Font font = LoadFontEx("./assets/Roboto-Regular.ttf", size_max, NULL, 95);
@@ -40,13 +49,39 @@ int main(void)
 
         bool isResized = IsWindowResized();
         if (isResized) {
-            win_width  = GetScreenWidth();
-            win_height = GetScreenHeight();
+            winWidth  = GetScreenWidth();
+            winHeight = GetScreenHeight();
         }
         ClearBackground(BLACK);
 
-        DrawFPS(10, 10);
+        switch(view) {
+            case UI_VIEW_START: {
+                Vector2 textSize = MeasureTextEx(font, startViewMsg, size_default, spacing);
+                Vector2 textPos  = { .x = (winWidth - textSize.x)/2.0f, .y = (winHeight - textSize.y)/2.0f };
+                DrawTextEx(font, startViewMsg, textPos, size_default, spacing, WHITE);
 
+                if (IsFileDropped()) {
+                    FilePathList droppedFiles = LoadDroppedFiles();
+                    if (droppedFiles.count > 1 || !IsPathFile(droppedFiles.paths[0])) {
+                        startViewMsg = "You can only drag and drop one MIDI-File to play it on the Piano.\nPlease try again";
+                    } else {
+                        view = UI_VIEW_FILE;
+                        u64 pathLen = strlen(droppedFiles.paths[0]);
+                        filePath = malloc(sizeof(char) * (pathLen + 1));
+                        memcpy(filePath, droppedFiles.paths[0], pathLen + 1);
+                    }
+                    UnloadDroppedFiles(droppedFiles);
+                }
+            } break;
+
+            case UI_VIEW_FILE: {
+                Vector2 textSize = MeasureTextEx(font, filePath, size_default, spacing);
+                Vector2 textPos  = { .x = (winWidth - textSize.x)/2.0f, .y = (winHeight - textSize.y)/2.0f };
+                DrawTextEx(font, filePath, textPos, size_default, spacing, WHITE);
+            } break;
+        }
+
+        // DrawFPS(10, 10);
         EndDrawing();
     }
 
