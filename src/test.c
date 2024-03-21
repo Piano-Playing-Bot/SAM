d86f9aa85d89355f81b403d14622407a03678ff0
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-03-21 12:19:07 +0100

    update test for pidi encode/decode

--- src/test.c
+++ src/test.c
@@ -6,8 +6,8 @@ bool cmd_eq(PidiCmd c1, PidiCmd c2)
 	return (
 		c1.key      == c2.key      &&
 		c1.octave   == c2.octave   &&
-		c1.on       == c2.on       &&
-		c1.time     == c2.time     &&
+		c1.dt       == c2.dt       &&
+		c1.len      == c2.len      &&
 		c1.velocity == c2.velocity
 	);
 }
@@ -17,36 +17,30 @@ int main(void)
 	AIL_Buffer buffer = ail_buf_new(64);
 	for (i8 octave = -8; octave < 8; octave++) {
 		for (u8 key = 0; key < PIANO_KEY_AMOUNT; key++) {
-			for (u8 velocity = 0; velocity < UINT8_MAX; velocity++) {
-				for (u8 on = 0; on < 2; on++) {
-					for (u64 time; time < 99999; time += 11) {
-						buffer.idx = 0;
-						buffer.len = 64;
-						buffer.cap = 64;
-						PidiCmd cmd = {
-							.key = key,
-							.octave = octave,
-							.on = (bool)on,
-							.velocity = velocity,
-							.time = time,
-						};
-						encode_cmd(&buffer, cmd);
-						PidiCmd new_cmd = decode_cmd_simple(buffer.data);
-						if (cmd_eq(cmd, new_cmd)) {
-							printf("Original Command: ");
-							print_cmd(cmd);
-							printf("\n");
-							printf("Decoded Command: ");
-							print_cmd(new_cmd);
-							printf("\n");
-							AIL_ASSERT(false);
-						}
-					}
+			for (u8 velocity = 0; velocity < MAX_VELOCITY; velocity++) {
+				buffer.idx = 0;
+				buffer.len = 64;
+				buffer.cap = 64;
+				PidiCmd cmd = {
+					.key = key,
+					.octave = octave,
+					.velocity = velocity,
+					.dt = 480,
+					.len = 20,
+				};
+				encode_cmd(&buffer, cmd);
+				PidiCmd new_cmd = decode_cmd_simple(buffer.data);
+				if (!cmd_eq(cmd, new_cmd)) {
+					printf("Original Command: ");
+					print_cmd(cmd);
+					printf("Decoded Command:  ");
+					print_cmd(new_cmd);
+					AIL_ASSERT(false);
 				}
 			}
 		}
 	}
 
-	printf("Test successful!\n");
+	printf("\033[32mTest successful!\033[0m\n");
 	return 0;
 }
