e5a12c4b0c477a84195b82d230b5d4ac8c49889b
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-03-27 23:52:47 +0100

    started work on issue #10

--- src/midi.c
+++ src/midi.c
@@ -13,12 +13,12 @@
 typedef AIL_DA(PidiCmd) PidiCmdList;
 AIL_DA_INIT(PidiCmdList);
 
-typedef union {
+typedef union ParseMidiResVal {
 	Song song;
 	char err[256];
 } ParseMidiResVal;
 
-typedef struct {
+typedef struct ParseMidiRes {
 	bool succ;
 	ParseMidiResVal val;
 } ParseMidiRes;
@@ -29,12 +29,18 @@ typedef struct PidiCmdTimed {
 } PidiCmdTimed;
 AIL_DA_INIT(PidiCmdTimed);
 
+typedef struct MidiTempos {
+    AIL_DA(u32) tempos;
+    AIL_DA(u32) times;
+    u32 cur_idx;
+} MidiTempos;
+
 #define MIDI_MAX_VELOCITY 127
 #define MIDI_MIN_CMD_SIZE 3 // at least delta_time: 1, key: 1, velocity: 1
 #define MIDI_0KEY_OCTAVE -5
 #define MIDI_NOTE_TO_OCTAVE(note) ((MIDI_0KEY_OCTAVE + ((note) / PIANO_KEY_AMOUNT)))
 #define MIDI_NOTE_TO_KEY(note)    ((note) % PIANO_KEY_AMOUNT)
-#define MIDI_TICKS_TO_MS(ticks, tempo, ticksPQN) ((ticks) * (u32)(((f32)(tempo) / (f32)(ticksPQN)) / 1000.0f))
+#define MIDI_TICKS_TO_MS(ticks, tempo, ticksPQN) ((u32)((f32)(ticks) * (((f32)(tempo) / (f32)(ticksPQN))/1000.0f)))
 
 u32 read_var_len(AIL_Buffer *buffer);
 ParseMidiRes parse_midi(AIL_Buffer buffer);
@@ -102,7 +108,6 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
     }
     buffer.idx += midiFileStartLen;
 
-    u32 tempo = 500000; // in ms. 500000ms = 120BPM (default value)
     u16 format   = ail_buf_read2msb(&buffer);
     u16 ntrcks   = ail_buf_read2msb(&buffer);
     u16 ticksPQN = ail_buf_read2msb(&buffer);
@@ -110,6 +115,14 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
         // If first bit is set, a different encoding is used for some reason
         AIL_TODO();
     }
+
+    MidiTempos tempos = {
+        .cur_idx = 0,
+        .times   = ail_da_new(u32),
+        .tempos  = ail_da_new(u32),
+    };
+    ail_da_push(&tempos.times, 0);
+    ail_da_push(&tempos.tempos, 500000);  // in ms. 500000ms = 120BPM (default value)
 
     // DBG_LOG("format: %d, ntrcks: %d, ticks per quarter-note: %d\n", format, ntrcks, ticksPQN);
 
@@ -120,20 +133,22 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
 
     AIL_DA(PidiCmdList) pidi_chunks = ail_da_new_with_cap(PidiCmdList, ntrcks);
     AIL_DA(u64)         start_times = ail_da_new_with_cap(u64, ntrcks);
-    for (u16 i = 0; i < ntrcks; i++) {
-        u8 command = 0; // used in running status (@Note: status == command)
-        u8 channel = 0; // used in running status
+    // for (u16 i = 0; i < ntrcks; i++) {
+    for (u16 i = 0; i < 2; i++) {
+        u8 command      = 0; // used in running status (@Note: status == command)
+        u8 channel      = 0; // used in running status
+        u32 cur_time    = 0;
         u32 last_cmd_dt = 0;
-        AIL_UNUSED(channel);
         // Parse track cmds
         AIL_ASSERT(ail_buf_read4msb(&buffer) == 0x4D54726B);
         u32 chunk_len   = ail_buf_read4msb(&buffer);
         u32 chunk_end   = buffer.idx + chunk_len;
         AIL_DA(PidiCmd) pidi_chunk = ail_da_new_with_cap(PidiCmd, chunk_len/MIDI_MIN_CMD_SIZE);
-        // DBG_LOG("Parsing cmd from %#010llx to %#010x\n", buffer.idx, chunk_end);
+        DBG_LOG("Parsing cmd from %#010llx to %#010x\n", buffer.idx, chunk_end);
         while (buffer.idx < chunk_end) {
             // Parse MTrk events
             u32 delta_time  = read_var_len(&buffer);
+            cur_time       += delta_time;
             last_cmd_dt    += delta_time;
             // DBG_LOG("index: %#010llx, delta_time: %d\n", buffer.idx, delta_time);
             if (ail_buf_peek1(buffer) == 0xff) {
@@ -167,10 +182,12 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
                     case 0x51: { // Set Tempo
                         AIL_ASSERT(ail_buf_read1(&buffer) == 3);
                         // u32 x = ail_buf_read3msb(&buffer);
-                        tempo = ail_buf_read3msb(&buffer);
-                        // DBG_LOG("tempo: %u\n", tempo);
+                        u32 new_tempo = ail_buf_read3msb(&buffer);
+                        ail_da_push(&tempos.tempos, new_tempo);
+                        ail_da_push(&tempos.times, cur_time);
+                        // DBG_LOG("tempo: %u, delta_time: %u\n", new_tempo, delta_time);
                     } break;
-                    case 0x54: { // SMPTE Offset
+                    case 0x54: { // SMPTE Offset - ignored
                         AIL_ASSERT(ail_buf_read1(&buffer) == 5);
                         u8 hr = ail_buf_read1(&buffer);
                         u8 mn = ail_buf_read1(&buffer);
@@ -178,18 +195,12 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
                         u8 fr = ail_buf_read1(&buffer);
                         u8 ff = ail_buf_read1(&buffer);
                         // > This event, if present, designates the SMPTE time at which the track cmd is supposed to start
-                        // @Study: Can we ignore this event?
-                        DBG_LOG("SMPTE - hour: %u, min: %u, sec: %u, fr: %u, ff: %u\n", hr, mn, se, fr, ff);
-                        AIL_TODO();
+                        DBG_LOG("\033[33mIgnoring SMPTE Offset - hour: %u, min: %u, sec: %u, fr: %u, ff: %u\033[0m\n", hr, mn, se, fr, ff);
                     } break;
                     case 0x58: { // Time Signature - ignored
                         AIL_ASSERT(ail_buf_read1(&buffer) == 4);
-                        buffer.idx += 2; // ignore num/den
-                        // ticksPQN = ail_buf_read1(&buffer);
-                        // u8 b = ail_buf_read1(&buffer);
-                        // DBG_LOG("b: %u\n", b); // @Bug
                         buffer.idx += 2;
-                        // AIL_ASSERT(b == 8); // It would be weird if there's not exactly 8 32nd notes ber quarter-note
+                        buffer.idx += 2;
                     } break;
                     case 0x59: { // Key Signature - ignored
                         AIL_ASSERT(ail_buf_read1(&buffer) == 2);
@@ -199,7 +210,7 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
                         u32 len = read_var_len(&buffer);
                         buffer.idx += len;
                     } break;
-                    default: {
+                    default: { // Non-MIDIv1-standard Meta Event - ignored
                         buffer.idx -= 2;
                         u16 ev = ail_buf_read2msb(&buffer);
                         DBG_LOG("\033[33mEncountered unknown meta event %#04x.\033[0m\n", ev);
@@ -224,16 +235,21 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
                         u8 note     = ail_buf_read1(&buffer);
                         u8 velocity = ail_buf_read1(&buffer);
                         i8 octave   = MIDI_NOTE_TO_OCTAVE(note);
-                        // DBG_LOG("octave: %d\n", octave);
                         u8 key      = MIDI_NOTE_TO_KEY(note);
+
+                        while (tempos.cur_idx < tempos.times.len && tempos.times.data[tempos.cur_idx] <= cur_time) tempos.cur_idx++;
+                        // u32 cur_tempo = tempos.tempos.data[tempos.tempos.len - 1];
+                        u32 cur_tempo = tempos.tempos.data[tempos.cur_idx];
+
+                        // DBG_LOG("Note on/off time offset: %d\n", MIDI_TICKS_TO_MS(last_cmd_dt, cur_tempo, ticksPQN));
                         if (command == 0x8 || !velocity) { // Note off
-                            u16 len     = MIDI_TICKS_TO_MS(last_cmd_dt, tempo, ticksPQN);
+                            u16 len = MIDI_TICKS_TO_MS(last_cmd_dt, cur_tempo, ticksPQN);
                             // DBG_LOG("Note off: key=%d, octave=%d, len=%d\n", key, octave, len);
                             for (u32 k = pidi_chunk.len - 1; k < pidi_chunk.len; k--) {
                                 PidiCmd *cmd = &pidi_chunk.data[k];
                                 if (pidi_key(*cmd) == key && pidi_octave(*cmd) == octave) {
                                     cmd->len = (len + LEN_FACTOR/2)/LEN_FACTOR; // +LEN_FACTOR/2 to do rounding
-                                    // DBG_LOG("Found note: ");
+                                    // DBG_LOG("\033[32mFound note: \033[0m");
                                     // print_cmd(*cmd);
                                     break;
                                 } else {
@@ -242,69 +258,78 @@ ParseMidiRes parse_midi(AIL_Buffer buffer)
                             }
                         } else { // Note on
                             PidiCmd cmd = {
-                                .dt       = MIDI_TICKS_TO_MS(last_cmd_dt, tempo, ticksPQN),
+                                .dt       = MIDI_TICKS_TO_MS(last_cmd_dt, cur_tempo, ticksPQN),
                                 .velocity = AIL_LERP((f32)velocity/MIDI_MAX_VELOCITY, 0, MAX_VELOCITY),
                                 .len      = 0,
                                 .octave   = octave,
                                 .key      = key,
                             };
                             ail_da_push(&pidi_chunk, cmd);
-                            // DBG_LOG("\033[32mNote on: \033[0m");
+                            // DBG_LOG("\033[33mNote on: \033[0m");
                             // print_cmd(cmd);
                             last_cmd_dt = 0;
                         }
                     } break;
-                    case 0xA: { // Polyphonic Key Pressure
-                        AIL_TODO();
+                    case 0xA: { // Polyphonic Key Pressure - ignored
+                        buffer.idx += 2;
                     } break;
                     case 0xB: { // Control Change
                         u8 c = ail_buf_read1(&buffer);
                         u8 v = ail_buf_read1(&buffer);
                         DBG_LOG("Control Change: c = %#01x, v = %#01x\n", c, v);
                         AIL_ASSERT(v <= 127);
-                        if (c < 120) {
+                        if (c < 120) { // ignored
                             // Do nothing for now
                             // @TODO: Check if any messages here might be interesting for us
                         } else switch (c) {
-                            case 120: { // All Sound off @TODO
-                                AIL_TODO();
-                            } break;
-                            case 121: { // Reset all controllers - ignored
-                            } break;
-                            case 122: {
-                                AIL_TODO();
-                            } break;
+                            case 120: // All Sound off - ignored
+                            case 121: // Reset all controllers - ignored
+                            case 122: // Local Control - ignored
+                                break;
                             case 123: { // All Notes off
-                                AIL_TODO();
+                                while (tempos.cur_idx < tempos.times.len && tempos.times.data[tempos.cur_idx] <= cur_time) tempos.cur_idx++;
+                                u32 cur_tempo = tempos.tempos.data[tempos.cur_idx];
+                                u16 len = MIDI_TICKS_TO_MS(last_cmd_dt, cur_tempo, ticksPQN);
+                                for (u32 k = pidi_chunk.len - 1; k < pidi_chunk.len; k--) {
+                                    PidiCmd *cmd = &pidi_chunk.data[k];
+                                    if (!pidi_len(*cmd)) {
+                                        cmd->len = (len + LEN_FACTOR/2)/LEN_FACTOR; // +LEN_FACTOR/2 to do rounding
+                                    } else {
+                                        len += cmd->dt;
+                                    }
+                                }
                             } break;
-                            case 124: {
-                                AIL_TODO();
-                            } break;
-                            case 125: {
-                                AIL_TODO();
-                            } break;
-                            case 126: {
-                                AIL_TODO();
-                            } break;
-                            case 127: {
-                                AIL_TODO();
-                            } break;
+                            case 124: // Omni Off - ignored
+                            case 125: // Omni On - ignored
+                            case 126: // Mono On (Poly Off) - ignored
+                            case 127: // Poly On (Mono Off) - ignored
+                                break;
                             default: AIL_UNREACHABLE();
                         }
                     } break;
                     case 0xC: { // Program Change - ignored
                         u8 patch = ail_buf_read1(&buffer);
                         DBG_LOG("Program Change: patch = %d\n", patch);
-                        // Do nothing
                     } break;
-                    case 0xD: { // Channel Pressure
-                        AIL_TODO();
+                    case 0xD: { // Channel Pressure - ignored
+                        buffer.idx += 1;
                     } break;
-                    case 0xE: { // Pitch Bend Change
+                    case 0xE: { // Pitch Bend Change - ignored
                         AIL_TODO();
+                        buffer.idx += 2;
                     } break;
-                    case 0xF: { // System Common Messages
-                        AIL_TODO();
+                    case 0xF: { // System Messages - ignored
+                        if (channel == 0) { // System Exclusive Message - ignored
+                            while (ail_buf_read1(&buffer) != 0xF7) {} // Read messages until reaching EOX (0xF7) message
+                        } else if (channel & 0x8) { // System Real Time Message - ignored
+                            // no data bytes to skip
+                            AIL_TODO();
+                        } else { // System Common Message - ignored
+                            DBG_LOG("channel: %d\n", channel);
+                            AIL_TODO();
+                            if (channel <= 3) buffer.idx += 1;
+                            // otherwise no data bytes to skip
+                        }
                     } break;
                 }
             }
@@ -345,10 +370,10 @@ void write_midi(Song song, const char *fpath)
         }
         AIL_SWAP_PORTABLE(PidiCmdTimed, cmds.data[min], cmds.data[i]);
     }
-    for (u32 i = 0; i < cmds.len; i++) {
-        printf("time: %4dms, ", cmds.data[i].time);
-        print_cmd(cmds.data[i].cmd);
-    }
+    // for (u32 i = 0; i < cmds.len; i++) {
+    //     printf("time: %4dms, ", cmds.data[i].time);
+    //     print_cmd(cmds.data[i].cmd);
+    // }
 
     write_timed_midi(cmds.data, cmds.len, fpath);
 }
@@ -358,7 +383,7 @@ void write_timed_midi(const PidiCmdTimed *cmds, u32 len, const char *fpath)
     // DBG_LOG("Writing %s back to midi in %s\n", song.name, fpath);
     AIL_Buffer buffer = ail_buf_new(2048);
     u16 ticksPQN = 480;
-    u32 tempo = 705882; // 500000;
+    u32 tempo = 500000;
     ail_buf_write1(&buffer, 'M');
     ail_buf_write1(&buffer, 'T');
     ail_buf_write1(&buffer, 'h');
