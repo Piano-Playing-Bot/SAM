c6b7d6c66e5b2dc2e0ab4b0257d9b3e6c2560071
ArtInLines <linesinart@gmail.com>
2024-04-12 16:41:20 +0200

    comply with updated SPPP

--- src/comm.c
+++ src/comm.c
@@ -30,7 +30,7 @@ typedef struct NextMsgRing {
     u8 end;
 } NextMsgRing;
 AIL_STATIC_ASSERT(NEXT_MSGS_COUNT <= UINT8_MAX);
-AIL_DA_INIT(MsgPidiPlayedKey);
+AIL_DA_INIT(PlayedKeySPPP);
 
 // @Note: All communication with the Arduino is done in a single thread external from the UI's main thread.
 // No other thread should write to these variables
@@ -43,13 +43,14 @@ static f32   comm_speed            = 1.0f;
 static u32   comm_time             = 0;
 static u32   comm_pidi_chunk_idx   = 0;
 static u32   comm_cmds_idx         = 0;
-static bool  comm_ignore_reqps     = false; // Indicates whether to ignore REQP messages for this loop iteration, because we just sent a new PIDI message
+static u16   comm_max_cmds_per_msg = 0;
+static bool  comm_ignore_requests  = false; // Indicates whether to ignore REQP messages for this loop iteration, because we just sent a new PIDI message
 static f64   last_comm_time        = 0.0f;  // Timestamp of last received message from Arduino - Only read_msg_fast write this value
 static ClientMsg comm_last_sent    = { 0 }; // Last message that was sent to the Arduino
 static AIL_RingBuffer comm_rb      = { 0 };
 static AIL_DA(PidiCmd) comm_cmds   = { 0 };
 static NextMsgRing comm_next_msgs  = { 0 };
-static AIL_DA(MsgPidiPlayedKey) comm_played_keys = { 0 };
+static AIL_DA(PlayedKeySPPP) comm_played_keys = { 0 };
 
 static pthread_mutex_t comm_volume_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t comm_speed_mutex  = PTHREAD_MUTEX_INITIALIZER;
@@ -77,11 +78,11 @@ ServerMsgType check_for_msg(void);
 void *comm_thread_main(void *args)
 {
     AIL_UNUSED(args);
-    comm_played_keys = ail_da_new_with_cap(MsgPidiPlayedKey, KEYS_AMOUNT);
+    comm_played_keys = ail_da_new_with_cap(PlayedKeySPPP, KEYS_AMOUNT);
     AIL_Allocator arena = ail_alloc_arena_new(2*AIL_ALLOC_PAGE_SIZE, &ail_alloc_pager);
     AIL_ASSERT(arena.data != NULL); // @TODO: Show error message if something goes wrong
     while (true) {
-        comm_ignore_reqps = false;
+        comm_ignore_requests = false;
         // If we are not connected, find port to connect
         if (ail_time_clock_elapsed(last_comm_time) >= MSG_TIMEOUT/1000.0f) {
             if (!comm_is_connected) {
@@ -109,36 +110,34 @@ void *comm_thread_main(void *args)
                     AIL_UNREACHABLE();
                     goto skip_sending_message;
                 case CMSG_PING:
-                case CMSG_CONT:
-                case CMSG_STOP:
+                case CMSG_CONTINUE:
                     msg = (ClientMsg) {
                         .type = next_msg,
-                        .data = { 0 },
+                        .data = { .b = !comm_is_paused },
                     };
                     break;
-                case CMSG_LOUD:
+                case CMSG_VOLUME:
                     msg = (ClientMsg) {
-                        .type = CMSG_LOUD,
+                        .type = CMSG_VOLUME,
                         .data = { .f = comm_volume },
                     };
                     break;
-                case CMSG_SPED:
+                case CMSG_SPEED:
                     msg = (ClientMsg) {
-                        .type = CMSG_SPED,
+                        .type = CMSG_SPEED,
                         .data = { .f = comm_speed },
                     };
                     break;
-                case CMSG_PIDI:
-                    comm_ignore_reqps = true;
+                case CMSG_NEW_MUSIC:
+                    comm_ignore_requests = true;
                     if (next_msgs_contain_pidi()) goto skip_sending_message;
-
                     u32 i = 0;
                     u32 prev_cmd_time = 0;
                     for (; i < comm_cmds.len && prev_cmd_time + comm_cmds.data[i].dt < comm_time; i++) {
                         PidiCmd cmd  = comm_cmds.data[i];
                         u32 end_time = prev_cmd_time + cmd.dt + cmd.len*LEN_FACTOR;
                         if (comm_time < end_time) {
-                            MsgPidiPlayedKey pk = {
+                            PlayedKeySPPP pk = {
                                 .key      = cmd.key,
                                 .octave   = cmd.octave,
                                 .len      = (end_time - comm_time)/LEN_FACTOR,
@@ -149,18 +148,19 @@ void *comm_thread_main(void *args)
                         prev_cmd_time += cmd.dt;
                     }
                     ClientMsgPidiData pidi = {
-                        .time        = comm_time,
-                        .cmds_count  = AIL_MIN(comm_cmds.len - i, CMDS_LIST_LEN),
-                        .cmds        = &comm_cmds.data[i],
-                        .idx         = 0,
                         .pks_count   = comm_played_keys.len,
                         .played_keys = comm_played_keys.data,
+                        .cmds_count  = AIL_MIN(comm_cmds.len - i, comm_max_cmds_per_msg),
+                        .cmds        = &comm_cmds.data[i],
                     };
                     comm_cmds_idx = i + pidi.cmds_count;
                     msg = (ClientMsg) {
-                        .type = CMSG_PIDI,
+                        .type = CMSG_MUSIC,
                         .data = { .pidi = pidi },
                     };
+                    break;
+                case CMSG_MUSIC:
+                    AIL_UNREACHABLE();
                     break;
             }
             comm_is_connected = send_msg(msg);
@@ -176,45 +176,36 @@ skip_sending_message:
                 res = check_for_msg();
                 switch (res) {
                     case SMSG_PONG:
-                    case SMSG_SUCC:
+                    case SMSG_SUCCESS:
                         switch (comm_last_sent.type) {
-                            case CMSG_PIDI:
+                            case CMSG_NEW_MUSIC:
+                            case CMSG_MUSIC:
                                 comm_is_music_playing = true;
-                                break;
-                            case CMSG_CONT:
-                                comm_is_paused = false;
-                                break;
-                            case CMSG_STOP:
-                                comm_is_paused = true;
                                 break;
                             default:
                                 break;
                         }
                         comm_last_sent = (ClientMsg){0}; // indicates, that last message was received successfully by arduino
                         break;
-                    case SMSG_REQP: {
-                        if (comm_ignore_reqps) continue;
+                    case SMSG_REQUEST: {
+                        if (comm_ignore_requests) continue;
                         while (pthread_mutex_lock(&comm_song_mutex) != 0) {}
-                        ClientMsg msg = { .type = CMSG_PIDI };
+                        ClientMsg msg = { .type = CMSG_MUSIC };
                         if (comm_cmds_idx < comm_cmds.len) {
                             msg.data.pidi = (ClientMsgPidiData) {
-                                .idx         = ++comm_pidi_chunk_idx,
-                                .time        = 0,
-                                .cmds_count  = AIL_MIN(comm_cmds.len - comm_cmds_idx, CMDS_LIST_LEN),
-                                .cmds        = &comm_cmds.data[comm_cmds_idx],
                                 .pks_count   = 0,
                                 .played_keys = comm_played_keys.data,
+                                .cmds_count  = AIL_MIN(comm_cmds.len - comm_cmds_idx, comm_max_cmds_per_msg),
+                                .cmds        = &comm_cmds.data[comm_cmds_idx],
                             };
-                            comm_cmds_idx += CMDS_LIST_LEN;
+                            comm_cmds_idx += comm_max_cmds_per_msg;
                         }
                         else {
                             msg.data.pidi = (ClientMsgPidiData) {
-                                .idx         = ++comm_pidi_chunk_idx,
-                                .time        = 0,
-                                .cmds_count  = 0,
-                                .cmds        = 0,
                                 .pks_count   = 0,
                                 .played_keys = comm_played_keys.data,
+                                .cmds_count  = 0,
+                                .cmds        = 0,
                             };
                         }
                         send_msg(msg);
@@ -245,12 +236,12 @@ ClientMsgType pop_msg(void)
     }
 }
 
-// Checks whether comm_next_msgs contains any CMSG_PIDI
+// Checks whether comm_next_msgs contains any CMSG_MUSIC or CMSG_NEW_MUSIC
 bool next_msgs_contain_pidi(void)
 {
     u8 n = (comm_next_msgs.end < comm_next_msgs.start)*NEXT_MSGS_COUNT + comm_next_msgs.end - comm_next_msgs.start;
     for (u8 i = 0, j = 0; i < n; i++, j = (j + 1)%NEXT_MSGS_COUNT) {
-        if (comm_next_msgs.data[j] == CMSG_PIDI) return true;
+        if (comm_next_msgs.data[j] == CMSG_MUSIC || comm_next_msgs.data[j] == CMSG_NEW_MUSIC) return true;
     }
     return false;
 }
@@ -263,15 +254,15 @@ void send_new_song(AIL_DA(PidiCmd) cmds, u32 start_time)
     comm_pidi_chunk_idx = 0;
     comm_time = start_time;
     comm_cmds = cmds;
-    push_msg(CMSG_PIDI);
+    push_msg(CMSG_NEW_MUSIC);
     while (pthread_mutex_unlock(&comm_song_mutex) != 0) {}
 }
 
 void set_paused(bool paused)
 {
     while (pthread_mutex_lock(&comm_volume_mutex) != 0) {}
-    if (paused) push_msg(CMSG_STOP);
-    else        push_msg(CMSG_CONT);
+    comm_is_paused = paused;
+    push_msg(CMSG_CONTINUE);
     while (pthread_mutex_unlock(&comm_volume_mutex) != 0) {}
 }
 
@@ -279,7 +270,7 @@ void set_volume(f32 volume)
 {
     while (pthread_mutex_lock(&comm_volume_mutex) != 0) {}
     comm_volume = volume;
-    push_msg(CMSG_LOUD);
+    push_msg(CMSG_VOLUME);
     while (pthread_mutex_unlock(&comm_volume_mutex) != 0) {}
 }
 
@@ -287,7 +278,7 @@ void set_speed(f32 speed)
 {
     while (pthread_mutex_lock(&comm_speed_mutex) != 0) {}
     comm_speed = speed;
-    push_msg(CMSG_SPED);
+    push_msg(CMSG_SPEED);
     while (pthread_mutex_unlock(&comm_speed_mutex) != 0) {}
 }
 
@@ -324,42 +315,34 @@ bool comm_setup_port(void) {
 // Read all incoming data from the port into the Ring Buffer
 void listen_to_port(void)
 {
-    #define READING_CHUNK_SIZE 16
+    #define READING_CHUNK_SIZE 8
 AIL_STATIC_ASSERT(READING_CHUNK_SIZE < AIL_RING_SIZE/2);
     u8 msg[READING_CHUNK_SIZE] = {0};
     DWORD read;
     bool res = ReadFile(comm_port, msg, READING_CHUNK_SIZE, &read, 0);
     if (comm_is_connected) comm_is_connected = res;
     ail_ring_writen(&comm_rb, (u8)read, msg);
-    // for (u8 i = 0; i < read; i++) printf("Read: %2x\n", msg[i]);
     for (u8 i = 0; i < read; i++) printf("%c", msg[i]);
+    // for (u8 i = 0; i < read; i++) printf("Read: %2x\n", msg[i]);
 }
 
 // Checks the Ring Buffer for any SPPP messages
 ServerMsgType check_for_msg(void)
 {
     // Go through Ring Buffer to check if any messages were received
-    // @TODO: Should Server always respond with the same message type instead of SUCC, so the client can know which message actually succeeded?
-    while (ail_ring_len(comm_rb) >= 12 && ail_ring_peek4msb(comm_rb) != SPPP_MAGIC) {
+    while (ail_ring_len(comm_rb) >= 4 && (ail_ring_peek4msb(comm_rb) & 0xffffff00) != SPPP_MAGIC) {
+        // printf("Popping off: '%c' (%d)\n", (char)ail_ring_peek(comm_rb), (int)ail_ring_peek(comm_rb))
         ail_ring_pop(&comm_rb);
     }
-    if (ail_ring_len(comm_rb) >= 12) {
-        ail_ring_popn(&comm_rb, 4);
-        ServerMsgType type = ail_ring_read4msb(&comm_rb);
-        u32 n = ail_ring_read4lsb(&comm_rb);
-        (void)n; // @TODO
-        #if 0
-        char *type_str;
-        switch (type) {
-            case SMSG_NONE: type_str = "NONE"; break;
-            case SMSG_REQP: type_str = "REQP"; break;
-            case SMSG_PONG: type_str = "PONG"; break;
-            case SMSG_SUCC: type_str = "SUCC"; break;
-            default:        type_str = "Unknown"; break;
+    if (ail_ring_peek_at(comm_rb, 3) == SMSG_PONG) {
+        if (ail_ring_len(comm_rb) >= 6) {
+            ail_ring_popn(&comm_rb, 4); // Remove magic bytes & type
+            comm_max_cmds_per_msg = ail_ring_read2lsb(&comm_rb);
         }
-        printf("Read message of type: %s\n", type_str);
-        #endif
-        return type;
+        return SMSG_PONG;
+    } else if (ail_ring_len(comm_rb) >= 4) {
+        ail_ring_popn(&comm_rb, 3); // Remove magic bytes
+        return (ServerMsgType)ail_ring_read(&comm_rb);
     }
     return SMSG_NONE;
 }
@@ -372,63 +355,57 @@ ServerMsgType wait_for_reply(void)
     while (ail_time_clock_elapsed(t) < (f64)MSG_TIMEOUT/1000.0f) {
         listen_to_port();
         ServerMsgType res = check_for_msg();
-        if (res != SMSG_NONE) return res;
+        if (res != SMSG_NONE) {
+            return res;
+
+        }
     }
     return SMSG_NONE;
 }
+
+#define MAX_CLIENT_MSG_SIZE (4 + 1 + 3*12*(1<<4) + 2 + 4*(1<<16))
 
 bool send_msg(ClientMsg msg)
 {
 #if 1
     char *msg_str;
     switch (msg.type) {
-        case CMSG_NONE: msg_str = "NONE"; break;
-        case CMSG_PING: msg_str = "PING"; break;
-        case CMSG_PIDI: msg_str = "PIDI"; break;
-        case CMSG_STOP: msg_str = "STOP"; break;
-        case CMSG_CONT: msg_str = "CONT"; break;
-        case CMSG_LOUD: msg_str = "LOUD"; break;
-        case CMSG_SPED: msg_str = "SPED"; break;
+        case CMSG_NONE:      msg_str = "NONE";      break;
+        case CMSG_PING:      msg_str = "PING";      break;
+        case CMSG_MUSIC:     msg_str = "MUSIC";     break;
+        case CMSG_NEW_MUSIC: msg_str = "NEW_MUSIC"; break;
+        case CMSG_CONTINUE:  msg_str = "CONTINUE";  break;
+        case CMSG_VOLUME:    msg_str = "VOLUME";    break;
+        case CMSG_SPEED:     msg_str = "SPEED";     break;
     }
     printf("Sending message of type %s to Arduino\n", msg_str);
 #endif
-    u8 msgBuffer[MAX_CLIENT_MSG_SIZE] = {0};
+    static u8 msgBuffer[MAX_CLIENT_MSG_SIZE] = {0};
     AIL_Buffer buffer = {
         .data = msgBuffer,
         .idx  = 0,
         .len  = 0,
         .cap  = MAX_CLIENT_MSG_SIZE,
     };
-    ail_buf_write4msb(&buffer, SPPP_MAGIC);
-    ail_buf_write4msb(&buffer, msg.type);
+    ail_buf_write4msb(&buffer, SPPP_MAGIC | msg.type);
     switch (msg.type) {
-        case CMSG_PIDI: {
-            ClientMsgPidiData pidi = msg.data.pidi;
-            if (pidi.idx == 0) {
-                // printf("size: %d\n",  4 + 4 + KEYS_AMOUNT + pidi.cmds_count * ENCODED_CMD_LEN);
-                // printf("max size: %lld\n", MAX_CLIENT_MSG_SIZE);
-                u32 size = 4 + 4 + 1 + pidi.pks_count*MSG_PIDI_PK_ENCODED_SIZE + pidi.cmds_count*ENCODED_CMD_LEN;
-                printf("size: %d\n", size);
-                ail_buf_write4lsb(&buffer, size);
-                ail_buf_write4lsb(&buffer, pidi.idx);
-                ail_buf_write4lsb(&buffer, pidi.time);
-                ail_buf_write1(&buffer, pidi.pks_count);
-                encode_played_keys(pidi.played_keys, pidi.pks_count, buffer.data);
-                buffer.idx += MSG_PIDI_PK_ENCODED_SIZE*pidi.pks_count;
-                for (u32 i = 0; i < pidi.cmds_count; i++) {
-                    encode_cmd(&buffer, pidi.cmds[i]);
-                }
-            } else {
-                ail_buf_write4lsb(&buffer, 4 + pidi.cmds_count * ENCODED_CMD_LEN);
-                ail_buf_write4lsb(&buffer, pidi.idx);
-                for (u32 i = 0; i < pidi.cmds_count; i++) {
-                    encode_cmd(&buffer, pidi.cmds[i]);
-                }
+        case CMSG_NEW_MUSIC:
+            ail_buf_write1(&buffer, msg.data.pidi.pks_count);
+            encode_played_keys(msg.data.pidi.played_keys, msg.data.pidi.pks_count, buffer.data);
+            buffer.idx += SPPP_PK_ENCODED_SIZE*msg.data.pidi.pks_count;
+            AIL_FALL_THROUGH();
+        case CMSG_MUSIC:
+            printf("Music with %d commands\n", msg.data.pidi.cmds_count);
+            ail_buf_write2lsb(&buffer, msg.data.pidi.cmds_count);
+            for (u32 i = 0; i < msg.data.pidi.cmds_count; i++) {
+                encode_cmd(&buffer, msg.data.pidi.cmds[i]);
             }
-        } break;
-        case CMSG_LOUD:
-        case CMSG_SPED:
-            ail_buf_write4lsb(&buffer, 4);
+            break;
+        case CMSG_CONTINUE:
+            ail_buf_write1(&buffer, (u8)msg.data.b);
+            break;
+        case CMSG_VOLUME:
+        case CMSG_SPEED:
             ail_buf_write4lsb(&buffer, *(u32 *)&msg.data.f);
             break;
         default:
@@ -441,7 +418,7 @@ bool send_msg(ClientMsg msg)
     // for (u8 i = 4; i < 8; i++) printf("%c", buffer.data[i]);
     // printf("' (%lld bytes)...\n", buffer.len);
     static int i = 0;
-    if (msg.type == CMSG_PIDI) {
+    if (msg.type == CMSG_MUSIC || msg.type == CMSG_NEW_MUSIC) {
         char buf[16];
         sprintf(buf, "msg-%d.buf", i++);
         ail_fs_write_file(buf, (char*)msgBuffer, buffer.len);
